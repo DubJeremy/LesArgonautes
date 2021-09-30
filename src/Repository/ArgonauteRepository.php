dd992cab98b88efed57418770aaa06035133ad48
DubJ <jeremy.ah33@gmail.com>
2021-09-30 10:48:12 +0200

    Clean code

--- src/Repository/ArgonauteRepository.php
+++ src/Repository/ArgonauteRepository.php
@@ -26,31 +26,4 @@ class ArgonauteRepository extends ServiceEntityRepository
     {
         return $this->findBy([], ['id' => 'DESC']);  
     }
-    
-    
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Argonaute
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
